0a74acf67cad3a9cf25b0dbe6003f4ad41658f30
Annette Maza <nettymaza@gmail.com>
2017-10-01 00:16:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
-  @@items = [Item.new("Shoes", "50"), Item.new("Pataloons", "100")]
   
+  @@items = [Item.new("Shoes", "50"), Item.new("Pataloons", "100")]
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
